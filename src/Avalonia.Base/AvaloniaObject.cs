5864e6c09522afc5f950d6723c44ecf90c0d4d36
Kasper Birkelund Larsen <kasperbirkelund@gmail.com>
2017-11-18 21:43:01 +0100

    kasper2

--- src/Avalonia.Base/AvaloniaObject.cs
+++ src/Avalonia.Base/AvaloniaObject.cs
@@ -54,7 +54,7 @@ namespace Avalonia
         /// <summary>
         /// Initializes a new instance of the <see cref="AvaloniaObject"/> class.
         /// </summary>
-        public AvaloniaObject(string whatever, int something, int newOne, int abc, object kasper, short s)
+        public AvaloniaObject(string whatever, int something, int newOne, int abc, object kasper, short s, int kasper2)
         {
             foreach (var property in AvaloniaPropertyRegistry.Instance.GetRegistered(this))
             {
